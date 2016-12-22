fb1bab1a747a3583fab846887d52376c6d923cdd
Chris Kippen <chriskippen@gmail.com>
2016-12-22 07:14:26 +0000

    Added custom contact form path

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,5 +1,5 @@
 class Contact < ActiveRecord::Base
-    validates :name, presence: true
+    validates :name, length: { minimum: 2 }
     validates :email, presence: true
     validates :comments, presence: true
 end
